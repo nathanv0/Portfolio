23314ee4e232a9f1ca756bc58ea51a6728297d24
Nathan Vo <115041909+nathanv0@users.noreply.github.com>
2025-10-12 22:29:39 -0500

    Add new File

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const words = [
+    {text: 'Idea', imgPath: '/images/ideas.svg'},
+    {text: 'Concept', imgPath: '/images/concepts.svg'},
+    {text: 'Designs', imgPath: '/images/designs.svg'},
+    {text: 'Code', imgPath: '/images/code.svg'},
+    {text: 'Idea', imgPath: '/images/ideas.svg'},
+    {text: 'Concept', imgPath: '/images/concepts.svg'},
+    {text: 'Designs', imgPath: '/images/designs.svg'},
+    {text: 'Code', imgPath: '/images/code.svg'}
+]
