c26ffe95ecba119b0994903fb2e7ad04a26ce2e7
Nathan Vo <115041909+nathanv0@users.noreply.github.com>
2025-10-16 04:32:16 -0500

    Init file

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,6 @@
 import {words} from "../constants/index.js";
+import Button from "../components/Button.jsx";
+
 
 const Hero = () => {
     return (
@@ -29,10 +31,13 @@ const Hero = () => {
                             <h1>that Deliver Results</h1>
                         </div>
                         <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">Hi, I'm Nathan, a developer based in MN with passion for code.</p>
+                        <Button className = "md:w-80 md:h-16 w-60 h-12"
+                        id="button" text="See my Work"/>
                     </div>
 
                 </header>
                 {/*RIGHT: 3D MODEL*/}
+
             </div>
 
         </section>
