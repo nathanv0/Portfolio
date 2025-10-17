069fbd5b834bfa035bef122841cbfab758167889
Nathan Vo <115041909+nathanv0@users.noreply.github.com>
2025-10-17 00:58:05 -0500

    Add Lighting

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,17 +1,15 @@
 import {Canvas} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 import {useMediaQuery} from "react-responsive";
+import {Room} from "./Room.jsx";
+import HeroLight from "./HeroLight.jsx";
 
 const HeroExperience = () => {
     const isTablet = useMediaQuery({query:"(max-width: 1024px)"});
-    /*
     const isMobile = useMediaQuery({query:"(max-width: 768px)"});
-     */
+
     return (
         <Canvas camera={{ position: [0, 0, 15], fov: 45}}>
-            <ambientLight intensity={0.2} color="#1a1a40"/>
-            <directionalLight position={[5,5,5]} intensity={1} />
-
             /* Move around the box */
             <OrbitControls
             enabledPan={false}
@@ -22,10 +20,15 @@ const HeroExperience = () => {
             maxPolarAngle={Math.PI / 2}
             />
 
-            <mesh>
-                <boxGeometry args={[1,1,1]} />
-                <meshStandardMaterial color="blue" />
-            </mesh>
+            <HeroLight />
+
+            <group scale={isMobile ? 0.7 : 1}
+            position={[0, -3.5, 0]}
+            /*Set default rotation for nice view*/
+            rotation={[0, -Math.PI / 4, 0]}>
+                <Room />
+            </group>
+
         </Canvas>
     )
 }
