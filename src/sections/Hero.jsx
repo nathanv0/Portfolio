e4dda8a74d47cddc53bef2e596ad345ecbf9d4ca
Nathan Vo <115041909+nathanv0@users.noreply.github.com>
2025-11-26 00:29:17 -0600

    Add AnimatedCounter and animation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,29 @@
 import {words} from "../constants/index.js";
 import Button from "../components/Button.jsx";
 import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
-import React from "react";
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
 
 
 const Hero = () => {
+    useGSAP(() => {
+        // UseGSAP for animation: target the hero-text h1 element
+        gsap.fromTo('.hero-text h1',
+            {
+                y: 50,
+                opacity: 0
+            },
+            {
+                y: 0,
+                opacity: 1,
+                stagger: 0.2,
+                duration: 2,
+                ease: 'power2.inOut'
+            },
+        )
+    })
+
     return (
         <section id="hero" className="relative overflow-hidden">
             <div className="absolute top-0 left-0 z-10">
@@ -46,7 +65,7 @@ const Hero = () => {
                 </figure>
 
             </div>
-
+            <AnimatedCounter />
         </section>
     )
 }
